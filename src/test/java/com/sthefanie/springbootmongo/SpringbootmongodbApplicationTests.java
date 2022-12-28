fc2cab6229be56801eb806eaed62142c9941ac7e
Sthefanie <sthefaniegc06@gmail.com>
2022-12-28 14:10:31 -0300

    First commit

---
+++ src/test/java/com/sthefanie/springbootmongo/SpringbootmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sthefanie.springbootmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
