ad93712a790464f802c9af51d308a5b44965d6f5
Sthefanie <sthefaniegc06@gmail.com>
2023-01-06 13:04:58 -0300

    Inserção de usuário com POST

--- src/main/java/com/sthefanie/springbootmongo/services/UserService.java
+++ src/main/java/com/sthefanie/springbootmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.sthefanie.springbootmongo.domain.User;
+import com.sthefanie.springbootmongo.dto.UserDTO;
 import com.sthefanie.springbootmongo.repository.UserRepository;
 import com.sthefanie.springbootmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+	
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
