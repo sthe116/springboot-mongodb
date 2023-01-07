94780ddc56206983335fc9152d341bc74ec1062d
Sthefanie <sthefaniegc06@gmail.com>
2023-01-07 16:12:59 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/sthefanie/springbootmongo/config/Instantiation.java
+++ src/main/java/com/sthefanie/springbootmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.sthefanie.springbootmongo.domain.Post;
 import com.sthefanie.springbootmongo.domain.User;
+import com.sthefanie.springbootmongo.dto.AuthorDTO;
 import com.sthefanie.springbootmongo.repository.PostRepository;
 import com.sthefanie.springbootmongo.repository.UserRepository;
 
@@ -35,13 +36,14 @@ public class Instantiation implements CommandLineRunner{
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem",
-				"Vou viajar para São Paulo. Abraços!", maria);
+				"Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 		
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", 
-				"Acordei feliz hoje!", maria);
+				"Acordei feliz hoje!", new AuthorDTO(maria));
 		
-		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 }
