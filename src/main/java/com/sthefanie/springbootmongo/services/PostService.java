f3a27e4f7ab50e19416053b80a2fae1a1cef2d5f
Sthefanie <sthefaniegc06@gmail.com>
2023-01-08 14:02:38 -0300

    Obtendo um post por id

---
+++ src/main/java/com/sthefanie/springbootmongo/services/PostService.java
@@ -0,0 +1,27 @@
+package com.sthefanie.springbootmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sthefanie.springbootmongo.domain.Post;
+import com.sthefanie.springbootmongo.repository.PostRepository;
+import com.sthefanie.springbootmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repository;
+	
+	public List<Post> findAll() {
+		return repository.findAll();
+	}
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+}
