af92ca2fe805bc8b7d8e3698a2352173fdfa4452
Sthefanie <sthefaniegc06@gmail.com>
2023-01-09 13:23:02 -0300

    Consulta simples com @Query

--- src/main/java/com/sthefanie/springbootmongo/repository/PostRepository.java
+++ src/main/java/com/sthefanie/springbootmongo/repository/PostRepository.java
@@ -3,12 +3,16 @@ package com.sthefanie.springbootmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.sthefanie.springbootmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }") // i -> IGNORARA LETRAS MAIUSC.
+	List<Post> searchTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text); 
 	//IgnoreCase -PARA IGNORAR LETRA MAUSCULA E MINUSCULA
